b078130840ce6070230e5b22e9f6b972a61f2c36
Radchenko Marina <mar.radchenko@gmail.com>
2026-05-29 12:11:58 +0300

    base front for choice

--- frontend-nuxt/app/pages/exercises/choice.vue
+++ frontend-nuxt/app/pages/exercises/choice.vue
@@ -9,8 +9,20 @@
           rows="1"
           class="w-full min-h-[3cm] lg:min-h-[2cm] max-h-40 overflow-y-auto px-4 py-2 border rounded-lg resize-none shadow-sm"
       />
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-3">
+        <textarea
+            v-model="option1Text"
+            :placeholder="$t('variant_1_placeholder')"
+            class="w-full overflow-y-auto px-4 py-2 border rounded-lg resize-none shadow-sm"
+        />
+        <textarea
+             v-model="option2Text"
+             :placeholder="$t('variant_2_placeholder')"
+             class="w-full overflow-y-auto px-4 py-2 border rounded-lg resize-none shadow-sm"
+        />
+      </div>
       <div class="flex flex-col md:flex-row gap-3 items-center">
-        <div class="text-start text-gray-600">{{$t('cards-number-per-time-period')}}</div>
+        <div class="text-start text-gray-600">{{$t('cards-number-per-option')}}</div>
         <div class="w-full md:w-[100px]">
           <select v-model.number="numberOfCards" class="w-full px-2 py-1 border rounded">
             <option value="1">1</option>
@@ -20,20 +32,20 @@
       </div>
       <div class="grid grid-cols-1" :class="{
         'grid-cols-1': numberOfCards !== 1,
-        'grid-cols-1 sm:grid-cols-3': numberOfCards === 1
+        'grid-cols-1 sm:grid-cols-2 gap-3': numberOfCards === 1
       }">
-        <div class="time-period-container" v-for="period in ['past', 'present', 'future']"
-             :key="period">
-          <h2 class="text-3xl font-bold my-3 text-primary text-center">{{$t(period)}}</h2>
+        <div class="option-cards-container" v-for="i in 2"
+             :key="i">
+          <h2 class="text-3xl font-bold my-3 text-primary text-center">{{$t("variant")}} {{i}}</h2>
           <template v-if="numberOfCards === 1">
             <div class="card-container flex justify-center">
-                <TurnCard :deck="deck" class="" v-model="cards[period][0]"/>
+              <TurnCard :deck="deck" class="" v-model="cards[`option_${i}`][0]"/>
             </div>
           </template>
           <template v-if="numberOfCards === 3">
             <div class="cards-row-container grid grid-cols-1 sm:grid-cols-3">
               <div class="card-container flex justify-center" :key="index" v-for="(n, index) in numberOfCards">
-                <TurnCard :deck="deck" class="" v-model="cards[period][index]"/>
+                <TurnCard :deck="deck" class="" v-model="cards[`option_${i}`][index]"/>
               </div>
             </div>
           </template>
@@ -74,17 +86,18 @@
 
 <script setup>
 const { t, locale} = useI18n()
-const { exercise } = useExercise('past-present-future')
+const { exercise } = useExercise('choice')
 const config = useRuntimeConfig()
 
 const loading = ref(false)
 const query = ref('')
+const option1Text = ref('')
+const option2Text = ref('')
 const numberOfCards = ref(1)
 const deck = ref('nature-reflections')
 const cards = ref({
-  past: [''],
-  present: [''],
-  future: ['']
+  option_1: [''],
+  option_2: [''],
 })
 const showIntelligentHintValidation = ref(false)
 const intelligentHint = ref({});
